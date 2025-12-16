e94712f4ed9f3b63bf2bdd22d72045e7f0fec7d5
Leeeydia <leeeydia@gmail.com>
2025-12-16 16:02:01 +0900

    SQLErrorException, DBUtil, SecSql 추가

--- db.sql
+++ db.sql
@@ -1,23 +1,71 @@
+DROP DATABASE IF EXISTS `JDBC_AM_25_12`;
 CREATE DATABASE `JDBC_AM_25_12`;
 USE `JDBC_AM_25_12`;
-SELECT DATABASE();
-SHOW TABLES;
-CREATE TABLE article (
-                         id INT (10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-                         regeDate DATETIME NOT NULL,
-                         updateDate DATETIME NOT NULL,
-                         title CHAR(100) NOT NULL,
-                         `body` TEXT NOT NULL
+
+CREATE TABLE article(
+                        id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+                        regDate DATETIME NOT NULL,
+                        updateDate DATETIME NOT NULL,
+                        title CHAR(100) NOT NULL,
+                        `body` TEXT NOT NULL
 );
-
-DESC article;
-
-SELECT * FROM article;
 
 INSERT INTO article
 SET regDate = NOW(),
-  updateDate = NOW(),
-  title = CONCAT ('제목1',SUBSTRING(RAND()*1000 frim 1 FOR 2)),
-  `body` = CONCAT('내용1',SUBSTRING(RAND()*1000 frim 1 FOR 2));
+updateDate = NOW(),
+title = '제목1',
+`body` = '내용1';
+
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = '제목2',
+`body` = '내용2';
+
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = '제목3',
+`body` = '내용3';
+
+
+DESC article;
+
+SELECT *
+FROM article;
+
+##===============================###################### 테스트
+
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = CONCAT('제목', SUBSTRING(RAND() * 1000 FROM 1 FOR 2)),
+`body` = CONCAT('내용', SUBSTRING(RAND() * 1000 FROM 1 FOR 2));
 
 SELECT NOW();
+
+SELECT '제목1';
+
+SELECT CONCAT('제목','2');
+
+SELECT SUBSTRING(RAND() * 1000 FROM 1 FOR 2);
+
+UPDATE article
+SET updateDate = NOW(),
+    title = '',
+    `body` = 'test1'
+WHERE id = 1;
+
+SELECT COUNT(*)
+FROM article
+WHERE id = 5;
+
+
+UPDATE article
+SET updateDate = NOW(),
+    `body` = 'test3'
+WHERE id = 3;
+
+
+SELECT *
+FROM article;
